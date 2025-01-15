da18dffb9d52c99fe74d8c8f90deb8cd6983b65d
TheMHarsh <harshm1804@gmail.com>
2025-01-15 22:26:30 +0530

    Improve Tic-Tac-Toe

--- ruby_projects/tic_tac_toe.rb
+++ ruby_projects/tic_tac_toe.rb
@@ -12,35 +12,34 @@ class Grid
         print '| ' if j != 2
       end
       puts
-      puts "---------" if i != 2 
+      puts "--+---+--" if i != 2 
     end
     puts
   end
 
   def move(mover)
     print "Enter which place do you want to change : "
-    move_location = gets.chomp.to_i
-    puts 
-    unless move_location.between?(1,9)
+    move_location = gets.to_i
+    puts
+    unless move_location.between?(1, 9)
       puts "Make a valid selection."
-      move(mover)
-      return
+      return move(mover)
     end
     i = ((move_location - 1) / 3)
-    j = (move_location % 3) - 1
-    if grid[i][j] != 'X' && grid[i][j] != 'O'  
-      grid[i][j] = mover
-    else
+    j = ((move_location - 1) % 3)
+    if ['X', 'O'].include?(grid[i][j]) 
       puts "Can't move there, please make a correct move this time."
-      move(mover)
+      return move(mover)
     end
+    grid[i][j] = mover
   end
 
   def check_win(mover)
-    if row_checker(mover) || diagonal_check(mover)
-      puts "#{mover} won the game" 
-      true
-    end
+    return unless row_checker(mover) || diagonal_check(mover)
+    
+    display
+    puts "#{mover} won the game" 
+    true
   end
 
   private
@@ -55,16 +54,14 @@ class Grid
   def diagonal_check(mover)
     flag = false
     i = 0
-    diag1 = grid.reduce([]) do |array, element| 
+    diag1 = grid.each_with_object([]) do |element, array| 
       array.push(element[i])
       i += 1
-      array
     end
     j = 2
-    diag2 = grid.reduce([]) do |array, element| 
+    diag2 = grid.each_with_object([]) do |element, array| 
       array.push(element[j])
       j -= 1
-      array
     end
     flag = true if diag1 == [mover, mover, mover] || diag2 == [mover, mover, mover]
     flag
@@ -76,21 +73,17 @@ def tic_tac_toe
   count = 0
   player = 'X'
   while count < 9
-    puts "#{player}'s chance -> \n"
+    puts "#{player}'s chance ->\n\n"
     grid.display
     grid.move(player)
     count += 1
     break if grid.check_win(player)
 
-    if player == 'X'
-      player = 'O'
-    elsif player == 'O'
-      player = 'X'
-    end
+    player = player == 'X' ? 'O' : 'X'
   end
   if count == 9 
     grid.display
-    puts "Nobody Won!"
+    puts "Nobody Won!" 
   end
   puts 'Game Over, Go Home!'
 end
