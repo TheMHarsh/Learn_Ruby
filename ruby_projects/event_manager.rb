624a7d764b11903d0f7aa8da1da7a22ce6e6a8f7
TheMHarsh <harshm1804@gmail.com>
2025-02-01 22:46:27 +0530

    Event Manager - Use CSV for parsing data

--- ruby_projects/event_manager.rb
+++ ruby_projects/event_manager.rb
@@ -1,8 +1,12 @@
+require 'csv'
 puts 'Event Manager Initialized!'
 
 if File.exist? 'ruby_projects/files/event_attendees.csv'
-  contents = File.read 'ruby_projects/files/event_attendees.csv'
-  puts contents
+  contents = CSV.read('ruby_projects/files/event_attendees.csv', headers: true, header_converters: :symbol)
+  contents.each do |row|
+    name = row[:first_name]
+    puts name
+  end 
 else
   puts 'No file found'
 end
