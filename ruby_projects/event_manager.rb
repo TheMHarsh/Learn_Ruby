242fc6dc8ba38d361360b43f86630ec547b9f333
TheMHarsh <harshm1804@gmail.com>
2025-02-03 22:33:28 +0530

    Event Manager - Clean Up Zipcodes

--- ruby_projects/event_manager.rb
+++ ruby_projects/event_manager.rb
@@ -10,3 +10,19 @@ if File.exist? 'ruby_projects/files/event_attendees.csv'
 else
   puts 'No file found'
 end
+
+# Cleaning Up the zip codes
+contents.each do |row|
+  if row[:zipcode].nil?
+    zipcode = '00000'
+  elsif row[:zipcode].length < 5
+    zipcode = row[:zipcode].rjust(5, '0')
+  elsif row[:zipcode].length > 5
+    zipcode = row[:zipcode][0..4]
+  else
+    zipcode = row[:zipcode]
+  end
+  puts zipcode
+end
+
+
